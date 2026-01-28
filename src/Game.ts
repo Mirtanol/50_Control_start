a37643b781eb28c31b305b8f2e8a4326eb27ada6
Григорий Медведко <vaflytv505@gmail.com>
2026-01-29 02:41:40 +0600

    task 16
    Что сделано:
    - Game.clone(): deep-copy steps через clone(), копирует input с сохранением прототипа, копирует boardParam и current.
    
    Зачем:
    - Сохранение/загрузка должны работать через независимые копии игры.

--- src/Game.ts
+++ src/Game.ts
@@ -35,9 +35,12 @@ export class Game<T extends GameType> {
     }
 
     clone(): Game<T> {
-        // TODO
-        // Функция должна вернуть копию объекта
-        return this 
+        const stepsCopy = this.steps.map((s) => s.clone())
+
+        const inputCopy = Object.create(Object.getPrototypeOf(this.input)) as Input<T>
+        Object.assign(inputCopy as any, this.input as any)
+
+        return new Game(stepsCopy, inputCopy, { ...this.boardParam }, this.current)
     }
 
     move(index: number): boolean {
