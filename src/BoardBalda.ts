33166efd3b99435794cee6db4b56f7e2482e1d18
Григорий Медведко <vaflytv505@gmail.com>
2026-01-29 02:39:29 +0600

    task 12
    Что сделано:
    - BoardBalda.constructor: слово длиной 5 дополняется 10 пробелами слева/справа.
    - Всегда формируется 25 символов (5x5).
    - При init задаются размеры 5x5 через родителя.
    
    Зачем:
    - Корректное поле для игры "Балда" и стабильная инициализация.

--- src/BoardBalda.ts
+++ src/BoardBalda.ts
@@ -6,13 +6,40 @@ export class BoardBalda extends Board<GameBalda> {
         str: string | Sym<GameBalda>[] = "балда",
         init: boolean = true
     ) {
-        // TODO
-        // Если длина str==5, то дополняет ее 10 пробелами до и после
-        // Вызывает родительский конструктор,
-        //  если init, то дополнительно инициализируются
-        //  статические поля класса
-        super({} as Sym<GameBalda>[])
+        const def = " ".repeat(10) + "балда" + " ".repeat(10)
+        let cells: Sym<GameBalda>[]
+
+        if (typeof str === "string") {
+            let s = str
+            if (s.length === 5) s = " ".repeat(10) + s + " ".repeat(10)
+            if (s.length !== 25) s = def
+            try {
+                cells = new Array(25)
+                for (let i = 0; i < 25; i++) cells[i] = new Sym<GameBalda>(s.charAt(i))
+            } catch {
+                cells = new Array(25)
+                for (let i = 0; i < 25; i++) cells[i] = new Sym<GameBalda>(def.charAt(i))
+            }
+        } else {
+            if (str.length === 5) {
+                const pad = new Array(10)
+                for (let i = 0; i < 10; i++) pad[i] = new Sym<GameBalda>(" ")
+                cells = [...pad, ...str, ...pad]
+            } else if (str.length === 25) {
+                cells = [...str]
+            } else {
+                cells = new Array(25)
+                for (let i = 0; i < 25; i++) cells[i] = new Sym<GameBalda>(def.charAt(i))
+            }
+        }
+
+        super(
+            cells,
+            init ? BoardBaldaParam.row : undefined,
+            init ? BoardBaldaParam.col : undefined
+        )
     }
+    
     clone(): BoardBalda {
         // TODO
         // Функция должна вернуть копию объекта
